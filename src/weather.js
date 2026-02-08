2cd9c0149b62ea3d72940d0bb3bf994f957c0447
sorikawolf <jennsorikawolf@gmail.com>
2026-02-07 22:19:41 -0500

    added search functionality using the API
    temperature only

--- src/weather.js
+++ src/weather.js
@@ -1,9 +1,27 @@
+function displayWeather(response) {
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temp-value");
+  let temperature = Math.round(response.data.temperature.current);
+
+  let cityTitle = document.querySelector("#current-city");
+  cityTitle.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+
+function searchCity(city) {
+  let apiKey = "357affaact442eeoc3f4199173062fb9";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
 function submitCity(event) {
   event.preventDefault();
-  let cityTitle = document.querySelector("#current-city");
   let cityInputElement = document.querySelector("#city-input");
-  cityTitle.innerHTML = cityInputElement.value;
+
+  searchCity(cityInputElement.value);
 }
 
 let searchFormElement = document.querySelector("#weather-search-form");
 searchFormElement.addEventListener("submit", submitCity);
+
+searchCity("Quebec");
