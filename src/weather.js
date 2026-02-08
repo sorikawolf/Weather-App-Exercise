264e200a1b51263c73de15dd04863a42deba92a3
sorikawolf <jennsorikawolf@gmail.com>
2026-02-08 14:53:15 -0500

    injected more weather data

--- src/weather.js
+++ src/weather.js
@@ -1,11 +1,23 @@
 function displayWeather(response) {
-  console.log(response.data.temperature.current);
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temp-value");
   let temperature = Math.round(response.data.temperature.current);
 
   let cityTitle = document.querySelector("#current-city");
   cityTitle.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
+
+  let descriptionElement = document.querySelector("#weather-description");
+  let description = response.data.condition.description;
+  descriptionElement.innerHTML = `${description}`;
+
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+  humidityElement.innerHTML = `<strong>${humidity}%</strong>`;
+
+  let windElement = document.querySelector("#wind-speed");
+  let windSpeed = response.data.wind.speed;
+  windElement.innerHTML = `<strong>${windSpeed} km/hr</strong>`;
 }
 
 function searchCity(city) {
@@ -23,5 +35,30 @@ function submitCity(event) {
 
 let searchFormElement = document.querySelector("#weather-search-form");
 searchFormElement.addEventListener("submit", submitCity);
+
+let date = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let dayElement = document.querySelector("#current-day");
+dayElement.innerHTML = `${days[date.getDay()]}`;
+
+let timeElement = document.querySelector("#time");
+let hours = date.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+timeElement.innerHTML = `${hours}:${minutes}`;
 
 searchCity("Quebec");
